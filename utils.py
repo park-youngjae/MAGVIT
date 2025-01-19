5c6124d9981278cba42584644c6a04dac78cc678
Youngjae Park <79900983+park-youngjae@users.noreply.github.com>
2025-01-19 16:18:02 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,8 +6,10 @@ from scipy.linalg import sqrtm
 import torch
 import torch.nn as nn
 
+
 def save_checkpoint(state, filename="checkpoint.pth.tar"):
     torch.save(state, filename)
+
 
 class EMA:
     def __init__(self, model, decay):
@@ -24,80 +26,3 @@ class EMA:
         for name, param in self.model.named_parameters():
             if name in self.shadow:
                 param.data.copy_(self.shadow[name])
-
-
-
-
-def trans(x):
-    # if greyscale images add channel
-    if x.shape[-3] == 1:
-        x = x.repeat(1, 1, 3, 1, 1)
-
-    # permute BTCHW -> BCTHW
-    x = x.permute(0, 2, 1, 3, 4) 
-
-    return x
-
-def calculate_fvd(videos1, videos2, device, method='styleganv', only_final=False):
-
-    if method == 'styleganv':
-        from fvd.styleganv.fvd import get_fvd_feats, frechet_distance, load_i3d_pretrained
-    elif method == 'videogpt':
-        from fvd.videogpt.fvd import load_i3d_pretrained, frechet_distance
-        from fvd.videogpt.fvd import get_fvd_logits as get_fvd_feats
-
-    print("calculate_fvd...")
-
-    # videos [batch_size, timestamps, channel, h, w]
-    
-    assert videos1.shape == videos2.shape
-
-    i3d = load_i3d_pretrained(device=device)
-    fvd_results = []
-
-    # support grayscale input, if grayscale -> channel*3
-    # BTCHW -> BCTHW
-    # videos -> [batch_size, channel, timestamps, h, w]
-
-    videos1 = trans(videos1)
-    videos2 = trans(videos2)
-
-    fvd_results = []
-
-    if only_final:
-
-        assert videos1.shape[2] >= 10, "for calculate FVD, each clip_timestamp must >= 10"
-
-        # videos_clip [batch_size, channel, timestamps, h, w]
-        videos_clip1 = videos1
-        videos_clip2 = videos2
-
-        # get FVD features
-        feats1 = get_fvd_feats(videos_clip1, i3d=i3d, device=device)
-        feats2 = get_fvd_feats(videos_clip2, i3d=i3d, device=device)
-
-        # calculate FVD
-        fvd_results.append(frechet_distance(feats1, feats2))
-    
-    else:
-
-        # for calculate FVD, each clip_timestamp must >= 10
-        for clip_timestamp in tqdm(range(10, videos1.shape[-3]+1)):
-        
-            # get a video clip
-            # videos_clip [batch_size, channel, timestamps[:clip], h, w]
-            videos_clip1 = videos1[:, :, : clip_timestamp]
-            videos_clip2 = videos2[:, :, : clip_timestamp]
-
-            # get FVD features
-            feats1 = get_fvd_feats(videos_clip1, i3d=i3d, device=device)
-            feats2 = get_fvd_feats(videos_clip2, i3d=i3d, device=device)
-        
-            # calculate FVD when timestamps[:clip]
-            fvd_results.append(frechet_distance(feats1, feats2))
-
-    result = {
-        "value": fvd_results,
-    }
-
-    return result
